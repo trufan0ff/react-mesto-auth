76eba0196cd3f981dbb34437b7ff019c623910c3
trufan0ff <trufan.off@yandex.ru>
2021-09-19 20:03:35 +0300

    исправляет ошибки после ревью

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -24,9 +24,9 @@ function AddPlacePopup(props) {
 
     return (
         <PopupWithForm onSubmit={handleSubmit} isOpen={props.isOpen} onClose={props.onClose} title={props.title}>
-            <input onChange={handleChangeName} className="popup__input popup__input_type_mesto-name" id="name" type="text" minLength="2" maxLength="30" required name="name" placeholder="Название" />
+            <input onChange={handleChangeName} value={name} className="popup__input popup__input_type_mesto-name" id="name" type="text" minLength="2" maxLength="30" required name="name" placeholder="Название" />
             <span className="name-error"></span>
-            <input onChange={handleChangeLink} className="popup__input popup__input_type_link" id="link" pattern="https://.*" type="url" name="link" required placeholder="Ссылка на картинку" />
+            <input onChange={handleChangeLink} value={link} className="popup__input popup__input_type_link" id="link" pattern="https://.*" type="url" name="link" required placeholder="Ссылка на картинку" />
             <span className="link-error"></span>
         </PopupWithForm>
     )
